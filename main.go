36df34ed35e91db3a2e84ed3faf48fc6f1acc8dc
Jia Supawit <supawit.dev@gmail.com>
2023-03-11 21:51:41 +0700

    Added missing services

--- main.go
+++ main.go
@@ -2,25 +2,113 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
+	var parkingLotSvc IParkingLotService = NewParkingLotService()
+	start(parkingLotSvc)
+}
+
+func start(parkingLotSvc IParkingLotService) {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Simple Shell")
-	fmt.Println("---------------------")
 
 	for {
-		fmt.Print("-> ")
 		text, _ := reader.ReadString('\n')
 		// convert CRLF to LF
 		// text = strings.Replace(text, "\n", "", -1)
-		text = strings.Replace(text, "\r\n", "", -1)
-
-		if strings.Compare("hi", text) == 0 {
-			fmt.Println("hello, Yourself")
+		text = strings.TrimSpace(strings.Replace(text, "\r\n", "", -1))
+		if strings.HasPrefix(text, "create_parking_lot") {
+			subStrings := strings.Split(text, " ")
+			if len(subStrings) != 2 {
+				fmt.Println(errors.New("invalid format"))
+				continue
+			}
+			slotAmount, err := strconv.Atoi(subStrings[1])
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			err = parkingLotSvc.CreateParkingLot(slotAmount)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+		} else if strings.HasPrefix(text, "park") {
+			subStrings := strings.Split(text, " ")
+			if len(subStrings) != 3 {
+				fmt.Println(errors.New("invalid format"))
+				continue
+			}
+			_, err := parkingLotSvc.Park(subStrings[1], subStrings[2])
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+		} else if strings.HasPrefix(text, "leave") {
+			subStrings := strings.Split(text, " ")
+			if len(subStrings) != 2 {
+				fmt.Println(errors.New("invalid format"))
+				continue
+			}
+			slotAmount, err := strconv.Atoi(subStrings[1])
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			err = parkingLotSvc.Leave(slotAmount)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+		} else if strings.HasPrefix(text, "status") {
+			err := parkingLotSvc.PrintStatus()
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+		} else if strings.HasPrefix(text, "registration_numbers_for_cars_with_colour") {
+			subStrings := strings.Split(text, " ")
+			if len(subStrings) != 2 {
+				fmt.Println(errors.New("invalid format"))
+				continue
+			}
+			err := parkingLotSvc.PrintParkedCarNumbersByColor(subStrings[1])
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+		} else if strings.HasPrefix(text, "slot_numbers_for_cars_with_colour") {
+			subStrings := strings.Split(text, " ")
+			if len(subStrings) != 2 {
+				fmt.Println(errors.New("invalid format"))
+				continue
+			}
+			err := parkingLotSvc.PrintParkedSlotNumbersByColor(subStrings[1])
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+		} else if strings.HasPrefix(text, "slot_number_for_registration_number") {
+			subStrings := strings.Split(text, " ")
+			if len(subStrings) != 2 {
+				fmt.Println(errors.New("invalid format"))
+				continue
+			}
+			err := parkingLotSvc.PrintParkedSlotNumberByCarNumber(subStrings[1])
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+		} else if strings.Contains(text, "exit") {
+			break
+		} else {
+			fmt.Println(errors.New("command not found"))
+			continue
 		}
 	}
 }
--- service.go
+++ service.go
@@ -3,12 +3,21 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 type IParkingLotService interface {
+	CreateParkingLot(slotAmount int) error
 	Park(carNumber string, carColor string) (int, error)
 	Leave(slotNumber int) error
-	GetStatus() error
+	PrintStatus() error
+	PrintParkedCarNumbersByColor(carColor string) error
+	PrintParkedSlotNumbersByColor(carColor string) error
+	PrintParkedSlotNumberByCarNumber(carNumber string) error
+
+	addCarNumberToColorCache(carNumber string, carColor string) error
+	removeCarNumberFromColorCache(carNumber string, carColor string) error
 	addSlotToColorCache(slotNumber int, carColor string) error
 	removeSlotFromColorCache(slotNumber int, carColor string) error
 	addSlotToCarNumberCache(slotNumber int, carNumber string) error
@@ -17,20 +26,36 @@ type IParkingLotService interface {
 
 type ParkingLotService struct {
 	parkingSlots          []*parkingSlot
+	carNumbersByColor     map[string]map[string]struct{}
 	slotNumbersByColor    map[string]map[int]struct{}
 	slotNumberByCarNumber map[string]int
 }
 
-func NewParkingLotService(slotAmount int) (*ParkingLotService, error) {
+func NewParkingLotService() *ParkingLotService {
+	// if slotAmount < 1 {
+	// 	return nil, errors.New("slot amount must be greater than 0")
+	// }
+
+	// return &ParkingLotService{
+	// 	parkingSlots:          make([]*parkingSlot, slotAmount),
+	// 	slotNumbersByColor:    map[string]map[int]struct{}{},
+	// 	slotNumberByCarNumber: map[string]int{},
+	// }
+
+	return &ParkingLotService{}
+}
+
+func (svc *ParkingLotService) CreateParkingLot(slotAmount int) error {
+	if len(svc.parkingSlots) > 0 {
+		return errors.New("parking lot was already created")
+	}
 	if slotAmount < 1 {
-		return nil, errors.New("slot amount must be greater than 0")
+		return errors.New("slot amount must be greater than 0")
 	}
 
-	return &ParkingLotService{
-		parkingSlots:          make([]*parkingSlot, slotAmount),
-		slotNumbersByColor:    map[string]map[int]struct{}{},
-		slotNumberByCarNumber: map[string]int{},
-	}, nil
+	svc.parkingSlots = make([]*parkingSlot, slotAmount)
+	fmt.Printf("Created a parking lot with %d slots\n", slotAmount)
+	return nil
 }
 
 func (svc *ParkingLotService) Park(carNumber string, carColor string) (int, error) {
@@ -41,26 +66,36 @@ func (svc *ParkingLotService) Park(carNumber string, carColor string) (int, erro
 		return 0, errors.New("car's color is empty")
 	}
 
+	carColor = strings.ToLower(carColor)
+
 	for idx, slot := range svc.parkingSlots {
 		if slot.parkedCar != nil {
 			continue
 		}
 
+		slotNumber := idx + 1
+
 		slot.parkedCar.registrationNumber = carNumber
 		slot.parkedCar.color = carColor
 
-		err := svc.addSlotToColorCache(idx, carColor)
+		err := svc.addCarNumberToColorCache(carNumber, carColor)
 		if err != nil {
 			slot.parkedCar = nil
 			return 0, errors.Join(err)
 		}
-		err = svc.addSlotToCarNumberCache(idx, carNumber)
+		err = svc.addSlotToColorCache(slotNumber, carColor)
+		if err != nil {
+			slot.parkedCar = nil
+			return 0, errors.Join(err)
+		}
+		err = svc.addSlotToCarNumberCache(slotNumber, carNumber)
 		if err != nil {
 			slot.parkedCar = nil
 			return 0, errors.Join(err)
 		}
 
-		return idx, nil
+		fmt.Printf("Allocated slot number: %d\n", slotNumber)
+		return slotNumber, nil
 	}
 
 	return 0, errors.New("parking lot is full")
@@ -71,7 +106,13 @@ func (svc *ParkingLotService) Leave(slotNumber int) error {
 		return errors.New("slot number is invalid")
 	}
 
-	err := svc.removeSlotFromColorCache(slotNumber, svc.parkingSlots[slotNumber-1].parkedCar.color)
+	carColor := svc.parkingSlots[slotNumber-1].parkedCar.color
+	carNumber := svc.parkingSlots[slotNumber-1].parkedCar.registrationNumber
+	err := svc.removeCarNumberFromColorCache(carNumber, carColor)
+	if err != nil {
+		return errors.Join(err)
+	}
+	err = svc.removeSlotFromColorCache(slotNumber, carColor)
 	if err != nil {
 		return errors.Join(err)
 	}
@@ -81,10 +122,12 @@ func (svc *ParkingLotService) Leave(slotNumber int) error {
 	}
 
 	svc.parkingSlots[slotNumber-1].parkedCar = nil
+	fmt.Printf("Slot number %d is free\n", slotNumber)
+
 	return nil
 }
 
-func (svc *ParkingLotService) GetStatus() error {
+func (svc *ParkingLotService) PrintStatus() error {
 	fmt.Println("Slot No.\tRegistration No\t\tColour")
 	for idx, slot := range svc.parkingSlots {
 		if slot.parkedCar == nil {
@@ -92,6 +135,103 @@ func (svc *ParkingLotService) GetStatus() error {
 		}
 
 		fmt.Printf("%d\t\t%s\t\t%s\n", idx, slot.parkedCar.registrationNumber, slot.parkedCar.color)
+	}
+
+	return nil
+}
+
+func (svc *ParkingLotService) PrintParkedCarNumbersByColor(carColor string) error {
+	if len(carColor) == 0 {
+		return errors.New("car's color is invalid")
+	}
+
+	cData, ok := svc.carNumbersByColor[carColor]
+	if !ok {
+		return errors.New("not found")
+	}
+
+	results := make([]string, 0, len(cData))
+	for carNumber := range cData {
+		if len(carNumber) == 0 {
+			continue
+		}
+		results = append(results, carNumber)
+	}
+	fmt.Println(strings.Join(results, ", "))
+
+	return nil
+}
+
+func (svc *ParkingLotService) PrintParkedSlotNumbersByColor(carColor string) error {
+	if len(carColor) == 0 {
+		return errors.New("car's color is invalid")
+	}
+
+	cData, ok := svc.slotNumbersByColor[carColor]
+	if !ok {
+		return errors.New("not found")
+	}
+
+	results := make([]string, 0, len(cData))
+	for slotNumber := range cData {
+		slotStr := strconv.Itoa(slotNumber)
+		if len(slotStr) == 0 {
+			continue
+		}
+		results = append(results, strconv.Itoa(slotNumber))
+	}
+	fmt.Println(strings.Join(results, ", "))
+
+	return nil
+}
+
+func (svc *ParkingLotService) PrintParkedSlotNumberByCarNumber(carNumber string) error {
+	if len(carNumber) == 0 {
+		return errors.New("car's number is invalid")
+	}
+
+	slotNumber, ok := svc.slotNumberByCarNumber[carNumber]
+	if !ok {
+		return errors.New("not found")
+	}
+
+	fmt.Println(slotNumber)
+
+	return nil
+}
+
+func (svc *ParkingLotService) addCarNumberToColorCache(carNumber string, carColor string) error {
+	if len(carNumber) == 0 {
+		return errors.New("car's number is empty")
+	}
+	if len(carColor) == 0 {
+		return errors.New("car's color is empty")
+	}
+
+	cData, ok := svc.carNumbersByColor[carColor]
+	if !ok {
+		svc.carNumbersByColor[carColor] = map[string]struct{}{
+			carNumber: {},
+		}
+	} else {
+		cData[carNumber] = struct{}{}
+		svc.carNumbersByColor[carColor] = cData
+	}
+
+	return nil
+}
+
+func (svc *ParkingLotService) removeCarNumberFromColorCache(carNumber string, carColor string) error {
+	if len(carNumber) == 0 {
+		return errors.New("car's number is empty")
+	}
+	if len(carColor) == 0 {
+		return errors.New("car's color is empty")
+	}
+
+	if cData, ok := svc.carNumbersByColor[carColor]; ok {
+		delete(cData, carNumber)
+		svc.carNumbersByColor[carColor] = cData
 	}
 
 	return nil
